0824117ab58b1f9bda3fb19b9b88122323f5dfac
rqds12 <rqds12@gmail.com>
2019-04-30 14:20:58 -0700

    added class player

---
+++ player.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sethsan on 4/30/19.
+//
+
+#include "player.h"
---
+++ player.h
@@ -0,0 +1,14 @@
+//
+// Created by sethsan on 4/30/19.
+//
+
+#ifndef CIT66FINALPROJ_PLAYER_H
+#define CIT66FINALPROJ_PLAYER_H
+
+
+class player {
+
+};
+
+
+#endif //CIT66FINALPROJ_PLAYER_H
