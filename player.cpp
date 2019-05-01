c3569210cb4a7c8be268f05477b9c4a638e8fac1
eriksan <unbz2012@gmail.com>
2019-05-01 09:37:51 -0700

    hhk

--- player.cpp
+++ player.cpp
@@ -2,4 +2,73 @@
 // Created by sethsan on 4/30/19.
 //
 
+#include <iostream>
 #include "player.h"
+#include <cstdlib>
+#include <ctime>
+
+void player::displayStatus() {
+
+}
+
+bool player::attack(player target) {
+
+    return false;
+}
+
+bool player::takeDamage(int dmg) {
+    return false;
+}
+
+bool player::specialAbility(player target) {
+    return false;
+}
+
+const string &player::getName() const {
+    return name;
+}
+void player::setName(const string &name) {
+    player::name = name;
+}
+const string &player::getWeapon() const {
+    return weapon;
+}
+void player::setWeapon(const string &weapon) {
+    player::weapon = weapon;
+}
+double player::getHealth() const {
+    return health;
+}
+void player::setHealth(double health) {
+    player::health = health;
+}
+const string &player::getResourceName() const {
+    return resourceName;
+}
+void player::setResourceName(const string &resourceName) {
+    player::resourceName = resourceName;
+}
+int player::getResource() const {
+    return resource;
+}
+void player::setResource(int resource) {
+    player::resource = resource;
+}
+int player::getArmor() const {
+    return armor;
+}
+void player::setArmor(int armor) {
+    player::armor = armor;
+}
+double player::getWeaponDmg() const {
+    return weaponDmg;
+}
+void player::setWeaponDmg(double weaponDmg) {
+    player::weaponDmg = weaponDmg;
+}
+int player::getAgility() const {
+    return agility;
+}
+void player::setAgility(int agility) {
+    player::agility = agility;
+}
--- player.h
+++ player.h
@@ -17,6 +17,44 @@ class player {
         int resource;
         int armor;
         double weaponDmg;
+        int agility;
+public:
+    void displayStatus();
+    bool attack(player target);
+    bool takeDamage(int dmg);
+    bool specialAbility(player target);
+
+    const string &getName() const;
+
+    void setName(const string &name);
+
+    const string &getWeapon() const;
+
+    void setWeapon(const string &weapon);
+
+    double getHealth() const;
+
+    void setHealth(double health);
+
+    const string &getResourceName() const;
+
+    void setResourceName(const string &resourceName);
+
+    int getResource() const;
+
+    void setResource(int resource);
+
+    int getArmor() const;
+
+    void setArmor(int armor);
+
+    double getWeaponDmg() const;
+
+    void setWeaponDmg(double weaponDmg);
+
+    int getAgility() const;
+
+    void setAgility(int agility);
 
 };
 
