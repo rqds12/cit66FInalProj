ca243cc7eed5066b6ab47ceec9c6b91e5f61e5d1
Eriksan <37591454+erikhearne@users.noreply.github.com>
2019-05-01 15:44:30 -0700

    added weapon name & type

--- Wizard.cpp
+++ Wizard.cpp
@@ -6,11 +6,12 @@
 
 Wizard::Wizard(string &name) {
     player::name = name;
-    player::weapon = "Wand, Dragon Heartstring, 10 &3/4 inches";
+    player::weapon = "Not the elder wand, but kinda scary still...";
     player::health = 159;
     player::resourceName = "Mana";
     player::resource = 40;
     player::armor =3;
+    player::weaponType = "Wand, Dragon Heartstring, 10 &3/4 inches";
     player::weaponDmg = 2.332;
 }
 
