4e9951bb5ae07c8690d7f1804837f9803010f01c
rqds12 <rqds12@gmail.com>
2019-05-01 09:05:47 -0700

    created a working build with ncurses cmake only needs set(CMAKE_CXXFLAGS "-lncurses")

--- main.cpp
+++ main.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
-#include <ncurses.h>
+
 
 int main() {
 
-    initscr();
-    printw("Try resizing your window(if possible) and then run this program again");
-    refresh();
-    getch();
-    endwin();
     return 0;
 }
