5970e4d1dbfa2d29c5a39fd09fea2cf4eee3a204
sethsan <rqds12@gmail.com>
2019-05-02 14:37:43 -0700

    added problem objects

---
+++ Problem.h
@@ -0,0 +1,14 @@
+//
+// Created by sethsan on 5/2/19.
+//
+
+#ifndef CIT66FINALPROJ_PROBLEM_H
+#define CIT66FINALPROJ_PROBLEM_H
+
+
+class Problem {
+
+};
+
+
+#endif //CIT66FINALPROJ_PROBLEM_H
--- main.cpp
+++ main.cpp
@@ -6,49 +6,11 @@
 #include "Rogue.h"
 
 void play(std::vector <player*> players);
-
+void startingMenu();
+void play();
 int main() {
-    std::vector <player*> players;
-    std::string temp="";
+    startingMenu();
 
-    std::cout << "Welcome to Text Adventure 3000\n" <<
-    "Please Select a class\n" <<
-    "1) Paladin\n" <<
-    "2) Wizard\n" <<
-    "3) Rogue\n";
-    getline(std::cin, temp);
-    switch(std::stoi(temp)){
-        case 1: {
-            std::cout << "Enter your name young Paladin\n";
-            getline(std::cin, temp);
-
-            auto user1 = new Paladin(temp);
-            players.push_back(user1);
-        }
-            break;
-        case 2: {
-            std::cout << "Enter your name young Wizard\n";
-            getline(std::cin, temp);
-
-            auto user2 = new Wizard(temp);
-            players.push_back(user2);
-        }
-            break;
-        case 3:{
-            std::cout << "Enter your name young Rogue\n";
-            getline(std::cin, temp);
-
-            auto user3 = new Rogue(temp);
-            players.push_back(user3);
-        }
-            break;
-        default:
-            std::cout << "You're a hooligan\n";
-            return 0;
-
-
-    }
-        play(players);
 
 
     return 0;
@@ -60,4 +22,51 @@ void play(std::vector<player*> players){
     for (int i = 0; i < story.size(); ++i) {
         std::cout << story[i] << "\n";
     }
+}
+void startingMenu(){
+    std::vector <player*> players;
+    std::string temp;
+    bool yeetusDeletus = false;
+
+    std::cout << "Welcome to Text Adventure 3000\n" <<
+              "Please Select a class\n" <<
+              "1) Paladin\n" <<
+              "2) Wizard\n" <<
+              "3) Rogue\n";
+    getline(std::cin, temp);
+    while (!yeetusDeletus) {
+        switch (std::stoi(temp)) {
+            case 1: {
+                std::cout << "Enter your name, young Paladin\n";
+                getline(std::cin, temp);
+
+                auto user1 = new Paladin(temp);
+                players.push_back(user1);
+                yeetusDeletus = true;
+            }
+                break;
+            case 2: {
+                std::cout << "Enter your name, young Wizard\n";
+                getline(std::cin, temp);
+
+                auto user2 = new Wizard(temp);
+                players.push_back(user2);
+                yeetusDeletus = true;
+            }
+                break;
+            case 3: {
+                std::cout << "Enter your name, young Rogue\n";
+                getline(std::cin, temp);
+
+                auto user3 = new Rogue(temp);
+                players.push_back(user3);
+                yeetusDeletus = true;
+            }
+                break;
+            default:
+                std::cout << "You're a hooligan\n";
+
+        }
+    }
+    play(players);
 }
