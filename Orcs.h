600842892c511786656708eac37f57cf4fbe0ede
rqds12 <rqds12@gmail.com>
2019-05-02 14:48:31 -0700

    Merge branch 'master' of /home/sethsan/CLionProjects/cit66FInalProj with conflicts.

---
+++ Orcs.h
@@ -0,0 +1,16 @@
+//
+// Created by sethsan on 5/2/19.
+//
+
+#ifndef CIT66FINALPROJ_ORCS_H
+#define CIT66FINALPROJ_ORCS_H
+
+
+#include "Enemy.h"
+
+class Orcs: public Enemy {
+
+};
+
+
+#endif //CIT66FINALPROJ_ORCS_H
