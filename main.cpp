21c1c30fd5d270522bea8c86ec674b8bbc9cc624
Eriksan <unbz2012@gmail.com>
2019-05-06 12:58:06 -0700

    memes test seth plz boss

--- main.cpp
+++ main.cpp
@@ -43,54 +43,15 @@ void startingMenu(){
               "Please Select a mode: \n"
               "1) Story Mode\n"
               "2) 2 Player" << endl;
+              int a = 0;
               getline(std::cin, temp);
               switch (std::stoi(temp)){
                   case 1:{
-                      cout << "Please Select a class\n" <<
-                           "1) Paladin\n" <<
-                           "2) Wizard\n" <<
-                           "3) Rogue\n";
-                      getline(std::cin, temp);
-                      while (!yeetusDeletus) {
-                          switch (std::stoi(temp)) {
-                              case 1: {
-                                  std::cout << "Enter your name, young Paladin\n";
-                                  getline(std::cin, temp);
-
-                                  auto user1 = new Paladin(temp);
-                                  players.push_back(user1);
-                                  yeetusDeletus = true;
-                              }
-                                  break;
-                              case 2: {
-                                  std::cout << "Enter your name, young Wizard\n";
-                                  getline(std::cin, temp);
-
-                                  auto user2 = new Wizard(temp);
-                                  players.push_back(user2);
-                                  yeetusDeletus = true;
-                              }
-                                  break;
-                              case 3: {
-                                  std::cout << "Enter your name, young Rogue\n";
-                                  getline(std::cin, temp);
-
-                                  auto user3 = new Rogue(temp);
-                                  players.push_back(user3);
-                                  yeetusDeletus = true;
-                              }
-                                  break;
-                              default:
-                                  std::cout << "You're a hooligan\n";
-                                  break;
-
-                          }
-                      }
-                      play(players);
-                      return;
+                      a = 1;
                   }
                   case 2:{
-                      for (int i = 0; i < 2; ++i) {
+                      a = 2;
+                      for (int i = 0; i < a; ++i) {
                           yeetusDeletus=false;
                           int ii=i+1;
                           cout << "Please Select a class for player " << ii << ": \n" <<
